d92efacbbd1779b45ddf3157a9acdfd452e2f7a0
Tony Fang <78616310+TFang96@users.noreply.github.com>
2026-03-04 09:19:07 -0800

    Naming conventions.

--- custom_fuzzy_k_means.R
+++ custom_fuzzy_k_means.R
@@ -53,12 +53,12 @@ custom_fuzzy_kmeans <- function(X, K, v = 2, improv_threshold = 1e-6, maxItr = 1
         # Using squared distances, the exponent is 1/(m-1)
         # u_ik = 1 / sum_j ( (d_ik / d_ij)^(1/(m-1)) )
         for (k in 1:K) {
-          U_new[i, k] <- 1 / sum((d_i[k] / d_i)^(1 / (m - 1)))
+          U_new[i, k] <- 1 / sum((d_i[k] / d_i)^(1 / (v - 1)))
         }
       }
     }
     
-    # --- compute fuzzy objective J_m
+    # --- compute fuzzy objective 
     E_new <- sum((U_new^v) * distances)
     
     # improvement (same sign convention as your code)
